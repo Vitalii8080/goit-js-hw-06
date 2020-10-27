a80f902c69f4220e79061264597190873dba4a65
Vitalii8080 <65123834+Vitalii8080@users.noreply.github.com>
2020-10-27 15:27:48 +0200

    1

---
+++ js/practice.js
@@ -0,0 +1,106 @@
+// const numbers = [1, 2, 3];
+
+// // Классический for
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(numbers[i]);
+// }
+
+// // Функциональный forEach
+// numbers.forEach(num => console.log(num));
+
+// // numbers.forEach(function (num) {
+// //   console.log(num);
+// // });
+
+// // Указываем параметр idx если нужен доступ к счетчику
+// numbers.forEach((num, idx) => console.log(`index ${idx}, value ${num}`));
+
+// const users = [
+//   { name: 'Mango', isActive: true },
+//   { name: 'Poly', isActive: false },
+//   { name: 'Ajax', isActive: true },
+//   { name: 'Chelsey', isActive: false },
+// ];
+
+// // Для каждого элемента коллекции (user) проверим поле isActive.
+// // Если оно true, то текущий элемент (user) будет записан в результирующий массив.
+// const activeUsers = users.filter(user => user.isActive);
+// console.log(activeUsers);
+
+// // Для каждого элемента коллекции (user) проверим поле isActive.
+// // Если оно false, то текущий элемент (user) будет записан в результирующий массив.
+// const inactiveUsers = users.filter(user => !user.isActive);
+// console.log(inactiveUsers);
+
+// const users = [
+//   { id: '000', name: 'Mango', isActive: true },
+//   { id: '001', name: 'Poly', isActive: false },
+//   { id: '002', name: 'Ajax', isActive: true },
+//   { id: '003', name: 'Chelsey', isActive: false },
+// ];
+
+// // Для каждого элемента коллекции (user) проверим поле id.
+// // Если оно совпадает с искомым идентификатором, то find прекратит
+// // выполнение и вернет текущий элемент (user) как результат выполнения
+// const user = users.find(user => user.id === '002');
+// console.log(user);
+
+// // Создадим функцию которая будет возвращать нам пользователя по id
+// const getUserById = (arr, id) => arr.find(x => x.id === id);
+
+// console.log(getUserById(users, '001'));
+// console.log(getUserById(users, '003'));
+
+// Callback-function
+// const printMessage = function (message) {
+//   console.log(message);
+// };
+
+// const higherOrderFunction = function (callback) {
+//   const string = 'HOCs are awesome';
+//   callback(string);
+// };
+
+// higherOrderFunction(printMessage);
+
+// const doMath = function (x, y, callback) {
+//   const result = callback(x, y);
+//   console.log(result);
+// };
+
+// const add = function (a, b) {
+//   return a + b;
+// };
+
+// const sub = function (a, b) {
+//   return a - b;
+// };
+
+// doMath(10, 5, add);
+// doMath(100, 80, sub);
+
+//map
+// const numbers = [1, 2, 3];
+
+// // const doubledMNumbers = numbers.map(function (number) {
+// //   return number * 2;
+// // });
+
+// const doubledMNumbers = numbers.map(number => number * 2);
+
+// console.log(doubledMNumbers);
+
+// const users = [
+//   { name: 'Mango', isActive: true },
+//   { name: 'Poly', isActive: false },
+//   { name: 'Ajax', isActive: true },
+// ];
+
+// const names = users.map(user => user.name);
+// console.log(names);
+
+// const arr = Array(3)
+//   .fill('')
+//   .map((e, i) => ({ ['index']: i }));
+
+// console.log(arr);
---
+++ js/tsk-10.js
@@ -0,0 +1,76 @@
+// Задача 6-10
+// reduce, filter, sort
+// Получи массив всех умений всех пользователей(поле skills),
+// при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+
+// Слияние массивов:
+
+// const odd = [1, 3, 5];
+// const even = [2, 4, 6];
+
+// // 1
+// [...odd, ...even];
+// //  [1, 3, 5, 2, 4, 6]
+
+// // 2
+// odd.concat(even)
+// //  [1, 3, 5, 2, 4, 6]
+// Используй только перебирающие методы массива которые не изменяют(не мутируют) исходный массив.
+// .е.нельзя использовать for, splice, push и т.п.мутирующие методы.
+
+// const users = [
+//   {
+//     id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ['ipsum', 'lorem'],
+//     gender: 'male',
+//     age: 37,
+//   },
+//   {
+//     id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+//     gender: 'male',
+//     age: 24,
+//   },
+//   {
+//     id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ['non', 'amet', 'ipsum'],
+//     gender: 'male',
+//     age: 38,
+//   },
+// ];
+
+// const getSortedUniqueSkills = array =>
+//   array
+//     .reduce((acc, { skills }) => [...acc, ...skills], [])
+//     // return array.reduce((acc, item) => [...acc, item.skills], []);
+//     .filter((current, index, arr) => arr.indexOf(current) === index)
+//     .sort();
+
+// console.log(getSortedUniqueSkills(users));
+
+// const array = [1, 3, 4, 6, 7, 7, 9, 8, 7];
+
+// console.log(
+//   array.filter((item, index) => {
+//     console.log(item, index, array.indexOf(item) === index);
+//     return array.indexOf(item) === index;
+//   }),
+// );
